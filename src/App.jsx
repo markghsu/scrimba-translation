5acd00ffc8ef14c67225c6e4c83f3ccb2ee0203a
Mark Hsu <mark.g.hsu@gmail.com>
2026-07-13 14:02:04 -0700

    Add finished translation screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import './App.css'
 import Parrot from './assets/parrot.png'
+import { useState } from "react"
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [translatedText, setTranslatedText] = useState("")
+  const [submittedTranslation, setSubmittedTranslation] = useState("")
+
+  async function handleSubmit(formData) {
+    const data = Object.fromEntries(formData)
+    console.log(data)
+    // API CALL HERE
+    setIsLoading(true)
+    setSubmittedTranslation(data["translation-input"])
+    try {
+      const response = await fetch('')
+      const res = await response.json()
+      setTranslatedText()
+    } 
+    catch (err) {
+      console.error(err)
+    }
+    finally {
+      setIsLoading(false)
+    }
+  }
+
+  function restart () {
+    setTranslatedText("")
+    setSubmittedTranslation("")
+  }
 
   return (
     <>
@@ -16,26 +44,37 @@ function App() {
         </div>
       </header>
       <main>
-        <form className="translation-form" id="translation-form">
-          <label className="input-label" htmlFor="translation-input">Text to translate 👇</label>
-          <textarea id="translation-input" name="translation-input" rows="4" defaultValue="How are you?" />
-          <fieldset id="language-radio">
-            <legend className="input-label">Select language 👇</legend>
-            <label>
-                <input type="radio" name="language" value="french"/>
-                French 🇫🇷
-            </label>
-            <label>
-                <input type="radio" name="language" value="spanish"/> 
-                Spanish 🇪🇸
-            </label>
+        { isLoading ? <h2>Loading...</h2> :
+          translatedText === "" ?
+          <form className="translation-form" id="translation-form" action={handleSubmit}>
+            <label className="input-label" htmlFor="translation-input">Text to translate 👇</label>
+            <textarea id="translation-input" name="translation-input" rows="4" required={true} defaultValue="How are you?" />
+            <fieldset id="language-radio">
+              <legend className="input-label">Select language 👇</legend>
               <label>
-                <input type="radio" name="language" value="japanese"/>
-                Japanese 🇯🇵
-            </label>
-          </fieldset>
-          <button className="submit-btn">Translate</button>
-        </form>
+                  <input type="radio" name="language" value="french" required={true} />
+                  French 🇫🇷
+              </label>
+              <label>
+                  <input type="radio" name="language" value="spanish"/> 
+                  Spanish 🇪🇸
+              </label>
+                <label>
+                  <input type="radio" name="language" value="japanese"/>
+                  Japanese 🇯🇵
+              </label>
+            </fieldset>
+            <button className="submit-btn">Translate</button>
+          </form>
+          :
+          <div className="translation-form" id="result">
+            <label className="input-label" htmlFor="translation-input">Original Text 👇</label>
+            <textarea name="translation-input" rows="4" readOnly={true} value={submittedTranslation} />
+            <label className="input-label" htmlFor="translation-output">Your Translation 👇</label>
+            <textarea name="translation-output" rows="4" readOnly={true} value={translatedText} />
+            <button className="submit-btn" onClick={restart}>Start Over</button>
+          </div>
+        }
       </main>
     </>
   )
