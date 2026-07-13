1ee170456b2ddb894de6bb986be2db78c5ff993e
Mark Hsu <mark.g.hsu@gmail.com>
2026-07-13 12:00:02 -0700

    Initial html/css created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import './App.css'
+import Parrot from './assets/parrot.png'
 
 function App() {
 
   return (
     <>
-      <section id="center">
-      </section>
+      <header className="header">
+        <img src={Parrot}
+          alt="PollyGlot Parrot" 
+          title="PollyGlot logo" 
+          className="logo" />
+        <div className="title-container">
+          <h1 className="title">PollyGlot</h1>
+          <p className="subtitle">Perfect Translation Every Time</p>
+        </div>
+      </header>
+      <main>
+        <form className="translation-form" id="translation-form">
+          <label className="input-label" htmlFor="translation-input">Text to translate 👇</label>
+          <textarea id="translation-input" name="translation-input" rows="4" defaultValue="How are you?" />
+          <fieldset id="language-radio">
+            <legend className="input-label">Select language 👇</legend>
+            <label>
+                <input type="radio" name="language" value="french"/>
+                French 🇫🇷
+            </label>
+            <label>
+                <input type="radio" name="language" value="spanish"/> 
+                Spanish 🇪🇸
+            </label>
+              <label>
+                <input type="radio" name="language" value="japanese"/>
+                Japanese 🇯🇵
+            </label>
+          </fieldset>
+          <button className="submit-btn">Translate</button>
+        </form>
+      </main>
     </>
   )
 }
